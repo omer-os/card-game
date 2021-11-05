9c92573b476c008e757e29247bf97f21c40ff119
omar chatin <omerchatin19@gmail.com>
2021-11-05 13:22:10 +0300

    added javascript logic

---
+++ app.js
@@ -0,0 +1,37 @@
+const imgs=[
+    "https://images.unsplash.com/photo-1635925088963-57affae93b97?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3Nnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1471897488648-5eae4ac6686b?ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHw2MHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1627110914363-5ecd867a8037?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1Nnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1636033358520-05e42539b449?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0OXx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1636063143596-d6fa6290ef60?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1635925088780-6c1c8e55a19e?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyOHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1636061455170-32ec066453f8?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxN3x8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "https://images.unsplash.com/photo-1635934753705-b803868bd1a0?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+]
+
+const RandomImg=()=>{
+    return imgs[Math.floor(Math.random(1,8)*8)]
+}
+
+let count=2;
+
+for(let i=1;i<=16;i++){
+    let cardImg=document.querySelector(`.card-${i}-img`)
+    cardImg.style=`background-image:url(${RandomImg()});`
+
+
+    let card=document.querySelector(`.card-${i}`)
+    card.addEventListener('click',()=>{
+        count+=1        
+        if(count>=2){
+            count=0
+            let AllCards=document.querySelectorAll('.card')
+            AllCards.forEach((card)=>{
+                card.classList.remove('open')
+            })
+            console.log(count);
+        }
+
+        card.classList.add('open')
+    })
+}
